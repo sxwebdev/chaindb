24534a0eea6fbf8860363f911f0bcaef0f5adcfa
sxwebdev <sxwebdev@gmail.com>
2025-06-09 15:10:13 +0300

    added DefaultPebbleLevelsWithCompression function

--- defaults.go
+++ defaults.go
@@ -14,3 +14,57 @@ var DefaultPebbleLevels = []pebble.LevelOptions{
 	{TargetFileSize: 64 * 1024 * 1024, FilterPolicy: bloom.FilterPolicy(10)},
 	{TargetFileSize: 128 * 1024 * 1024, FilterPolicy: bloom.FilterPolicy(10)},
 }
+
+func DefaultPebbleLevelsWithCompression(compressionType pebble.Compression) []pebble.LevelOptions {
+	return []pebble.LevelOptions{
+		{
+			TargetFileSize: 2 * 1024 * 1024,
+			FilterPolicy:   bloom.FilterPolicy(10),
+			Compression: func() pebble.Compression {
+				return compressionType
+			},
+		},
+		{
+			TargetFileSize: 4 * 1024 * 1024,
+			FilterPolicy:   bloom.FilterPolicy(10),
+			Compression: func() pebble.Compression {
+				return compressionType
+			},
+		},
+		{
+			TargetFileSize: 8 * 1024 * 1024,
+			FilterPolicy:   bloom.FilterPolicy(10),
+			Compression: func() pebble.Compression {
+				return compressionType
+			},
+		},
+		{
+			TargetFileSize: 16 * 1024 * 1024,
+			FilterPolicy:   bloom.FilterPolicy(10),
+			Compression: func() pebble.Compression {
+				return compressionType
+			},
+		},
+		{
+			TargetFileSize: 32 * 1024 * 1024,
+			FilterPolicy:   bloom.FilterPolicy(10),
+			Compression: func() pebble.Compression {
+				return compressionType
+			},
+		},
+		{
+			TargetFileSize: 64 * 1024 * 1024,
+			FilterPolicy:   bloom.FilterPolicy(10),
+			Compression: func() pebble.Compression {
+				return compressionType
+			},
+		},
+		{
+			TargetFileSize: 128 * 1024 * 1024,
+			FilterPolicy:   bloom.FilterPolicy(10),
+			Compression: func() pebble.Compression {
+				return compressionType
+			},
+		},
+	}
+}
